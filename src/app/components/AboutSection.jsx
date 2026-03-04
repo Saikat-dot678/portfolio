0d8f3e6936bd1510aaa3051b22e890a663dd82b0
Saikat-dot678 <saikatkhan2005saikat@gmail.com>
2026-03-04 07:38:13 +0530

    change in Hero , Bio and footer

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -57,12 +57,15 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
+            {/* I am a full stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
             working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
             Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+            I am excited to work with others to create amazing applications. */}
+            I’m a Computer Science undergraduate at NIT Durgapur passionate about building secure and reliable systems for real-world environments. My interests lie in backend engineering, embedded systems, networking, and distributed architectures.
+
+            I’ve developed full-stack platforms with secure role-based access control, engineered multi-factor attendance systems with geofencing, and built custom LoRa transport layers using Selective Repeat ARQ for reliable long-range communication. I enjoy solving problems that require both system-level design and low-level implementation.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
